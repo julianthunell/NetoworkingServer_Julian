a0bb026d1e6a9cb7f2efb4630356abde5b0eba5a
julianthunell <123653265+julianthunell@users.noreply.github.com>
2024-05-06 10:20:13 +0200

    asfasdf

--- src/Main.java
+++ src/Main.java
@@ -23,7 +23,28 @@ public class Main {
         System.out.println("ServerSocket is created" + server);
 
         //Recieve data from client
+        while (true){
+            try{
+                //Listens for a connection
+                System.out.println("Waiting for connect request...");
+                client = server.accept();
 
+                System.out.println("Connect request is accepted...");
+                String clientHost = client.getInetAddress().getHostAddress();
+
+                int clientPort = client.getPort();
+                System.out.println("Client host = " + clientHost + "client port = " + clientPort);
+
+                //Read data from client
+                InputStream clientIn = client.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(clientIn));
+
+                String messageFromClient = br.readLine();
+                System.out.println("Message recieved from client = "); messageFromClient)
+
+
+            }
+        }
 
     }
 }
